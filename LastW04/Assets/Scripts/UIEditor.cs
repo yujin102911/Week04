81aaf17a21bfbdc7aa84442a2f87694d7b96fd57
GameLab4th_leejungmin <lkswasd@gmail.com>
2025-09-26 19:43:02 +0900

    수정

--- LastW04/Assets/Scripts/UIEditor.cs
+++ LastW04/Assets/Scripts/UIEditor.cs
@@ -5,7 +5,7 @@ public class UIEditor : MonoBehaviour
 {
     public Grid grid;
     public GameObject editingUI;//하단OnOFF용
-    Transform draggingInstance;
+    public Transform draggingInstance;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -23,63 +23,68 @@ public class UIEditor : MonoBehaviour
         {
             editingUI.SetActive(false);//에딧UI 가림막 참으로
         }
+
         if (GameManager.mode == Mode.Editing)//에디팅 모드 시 편집 기능
         {
-            if (Input.GetMouseButtonDown(0))
+            if (Input.GetMouseButtonDown(0))//좌클 시
             {
                 // 마우스 아래 오브젝트 감지
                 Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
                 RaycastHit2D hit = Physics2D.GetRayIntersection(ray);
                 if (hit.collider != null && hit.collider.CompareTag("EditorbleUI"))
                 {
-                    draggingInstance = hit.collider.transform;
-                }
-            }
-
-            if (Input.GetMouseButtonDown(1))
-            {
-                // 마우스 아래 오브젝트 감지
-                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-                RaycastHit2D hit = Physics2D.GetRayIntersection(ray);
-                if (hit.collider != null)
-                {
-                    if (hit.collider != null && hit.collider.CompareTag("EditorbleUI"))
+                    if (hit.transform.parent != null)
                     {
-                        if (hit.transform.parent != null)
-                        {
-                            Destroy(hit.transform.parent.gameObject);
-                        }
-                        else
-                        {
-                            Destroy(hit.collider.gameObject);
-                        }
-
+                        draggingInstance=hit.transform.parent;
+                    }
+                    else
+                    {
+                        draggingInstance=hit.transform;
                     }
                 }
             }
 
-            if (Input.GetMouseButton(0) && draggingInstance != null)//드래그중일때
+            if (Input.GetMouseButtonDown(1))//우클 시 삭제
             {
-                if (draggingInstance == null) return;
-                //놓은 곳에 아무것도 없으면
+                // 마우스 아래 오브젝트 감지
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                RaycastHit2D hit = Physics2D.GetRayIntersection(ray);
+                if (hit.collider != null && hit.collider.CompareTag("EditorbleUI"))
+                {
+                    if (hit.transform.parent != null)
+                    {
+                        Destroy(hit.transform.parent.gameObject);
+                    }
+                    else
+                    {
+                        Destroy(hit.collider.gameObject);
+                    }
+                }                
+            }
+
+            if (Input.GetMouseButton(0) && draggingInstance != null)//좌클 드래그중일때
+            {
+                Vector2 screenPos = Input.mousePosition;
+                Vector3 worldPos = Camera.main.ScreenToWorldPoint(screenPos);
+                // 그리드에 스냅
+                Vector3Int cell = grid.WorldToCell(worldPos);
+                Vector3 aligned = grid.CellToWorld(cell);
+                draggingInstance.transform.position = aligned + Vector3.back;
+            }
+            if(Input.GetMouseButtonUp(0) && draggingInstance != null)
+            {   //놓은 곳에 아무것도 없으면
                 var hits = Physics2D.OverlapBoxAll(transform.position, new Vector2(1, 1), 0);
                 foreach (var h in hits)
                 {
                     if (h.CompareTag("EditorbleUI"))//다른 UI있으면
                     {
+                        draggingInstance = null;
                         return;//밑에 코드 실행 ㄴㄴ
                     }
                 }
-                // 화면 좌표 → 월드 좌표
-                Vector3 screenPos = Mouse.current.position.ReadValue();
-                Vector3 worldPos = Camera.main.ScreenToWorldPoint(screenPos);
-                worldPos.z = -1f; // 2D용 Z값
-
-                // 그리드에 스냅
-                Vector3Int cell = grid.WorldToCell(worldPos);
-                Vector3 aligned = grid.CellToWorld(cell);
-                draggingInstance.transform.position = aligned + Vector3.back;
                 draggingInstance = null;
+
+
             }
         }
     }
