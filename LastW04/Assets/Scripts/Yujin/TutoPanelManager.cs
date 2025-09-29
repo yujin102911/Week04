da17f21d66cef32d27774758838fe0d70c1bd423
박유진 <yujin102911@gmail.com>
2025-09-29 16:59:05 +0900

    애니메이션 수정

---
+++ LastW04/Assets/Scripts/Yujin/TutoPanelManager.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using System.Collections; // 코루틴을 사용하기 위해 필요합니다.
+
+
+public class TutoPanelManager : MonoBehaviour
+{
+    [SerializeField] private GameObject panel1;
+    [SerializeField] private GameObject panel2;
+    [SerializeField] private GameObject panel3;
+
+    [SerializeField] private float switchDelay = 1.0f;
+
+    private bool isSwitching = false;
+
+    public void SwitchPanel1To2()
+    {
+        if (!isSwitching)
+            StartCoroutine(SwitchPanels(panel1, panel2));
+    }
+
+    public void SwitchPanel2To3()
+    {
+        if (!isSwitching)
+            StartCoroutine(SwitchPanels(panel2, panel3));
+    }
+
+    public void DisablePanel3()
+    {
+        if (panel3 != null)
+            panel3.SetActive(false);
+    }
+
+    private IEnumerator SwitchPanels(GameObject panelToDisable, GameObject panelToEnable)
+    {
+        isSwitching = true;
+        Debug.Log($"{switchDelay}초 뒤에 패널 전환!");
+
+        yield return new WaitForSeconds(switchDelay);
+
+        if (panelToDisable != null)
+            panelToDisable.SetActive(false);
+
+        if (panelToEnable != null)
+            panelToEnable.SetActive(true);
+
+        Debug.Log("패널 전환 완료!");
+        isSwitching = false;
+    }
+
+}
